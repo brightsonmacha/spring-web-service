7e9e857ecba21fd27710ab6b24363f0df3a5b7fd
Brightson Macha <brymacha065@gmail.com>
2023-02-10 20:14:26 +0300

    add dummy country

--- src/main/java/com/brightsonmacha/soapwsdlwebservice/repos/CountryRepository.java
+++ src/main/java/com/brightsonmacha/soapwsdlwebservice/repos/CountryRepository.java
@@ -1,6 +1,7 @@
 package com.brightsonmacha.soapwsdlwebservice.repos;
 
 import com.brightsonmacha.generated.country.Country;
+import com.brightsonmacha.generated.country.Currency;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -15,6 +16,30 @@ public class CountryRepository {
     @PostConstruct
     public void initData() {
         // initialize countries map
+        Country country = new Country();
+
+        country.setName("tanzania");
+        country.setCapital("Dar es Saalam");
+        country.setCurrency(Currency.PLN);
+        countries.put(country.getName(), country);
+
+        country = new Country();
+        country.setName("uganda");
+        country.setCapital("Kampala");
+        country.setCurrency(Currency.PLN);
+        countries.put(country.getName(), country);
+
+        country = new Country();
+        country.setName("kenya");
+        country.setCapital("Nairobi");
+        country.setCurrency(Currency.PLN);
+        countries.put(country.getName(), country);
+
+        country = new Country();
+        country.setName("burundi");
+        country.setCapital("Kigali");
+        country.setCurrency(Currency.PLN);
+        countries.put(country.getName(), country);
     }
 
     public Country findCountry(String name) {
